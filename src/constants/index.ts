7f4ad11427ece82fc895581b8b13757468593f28
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2025-12-13 19:56:08 +0800

    feat: setup tabs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,5 +6,5 @@ export const MODE_TEXT = {
 export const BLACK_COLOR = "#000000";
 
 export const INFO_TAB = { id: 1, text: "基本資料" };
-export const DETAIL_TAB = { id: 1, text: "明細" };
-export const CHECKOUT_TAB = { id: 1, text: "結帳" };
+export const DETAIL_TAB = { id: 2, text: "明細" };
+export const CHECKOUT_TAB = { id: 3, text: "結帳" };
--- src/pages/home/projectDetail/index.vue
+++ src/pages/home/projectDetail/index.vue
@@ -2,22 +2,12 @@
   <Title :title />
   <div class="my-8"></div>
 
-  {{ tab }}
-  <Tabs v-model="tab" />
+  <v-tabs v-model="tab" align-tabs="start" color="deep-purple-accent-4">
+    <v-tab v-for="tab in TABS" :value="tab.id">{{ tab.text }}</v-tab>
+  </v-tabs>
   <v-tabs-window v-model="tab">
     <v-tabs-window-item v-for="n in 3" :key="n" :value="n">
-      <v-container fluid>
-        <v-row>
-          <v-col v-for="i in 6" :key="i" cols="12" md="4">
-            <v-img
-              :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
-              :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
-              height="205"
-              cover
-            ></v-img>
-          </v-col>
-        </v-row>
-      </v-container>
+      {{ n }}
     </v-tabs-window-item>
   </v-tabs-window>
 </template>
@@ -26,6 +16,7 @@ import Title from "@/components/Title/index.vue";
 import Tabs from "./components/Tabs";
 import { INFO_TAB, DETAIL_TAB, CHECKOUT_TAB } from "@/constants";
 
+const TABS = [INFO_TAB, DETAIL_TAB, CHECKOUT_TAB];
 const route = useRoute();
 const id = computed(() => route.query.id);
 const isNew = computed(() => id.value === "new");
@@ -36,5 +27,5 @@ const title = computed(() => {
     return "編輯專案";
   }
 });
-const tab = ref(1);
+const tab = ref(INFO_TAB.id);
 </script>
